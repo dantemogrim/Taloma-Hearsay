234c82fd5fcdea68ce991cbad67610a2f169d17c
Dante Mogrim <1991.mogrim@gmail.com>
2021-02-06 14:32:37 +0100

    All CSS through media queries?

--- index.php
+++ index.php
@@ -4,41 +4,42 @@
 
 <main>
 
-    <?php foreach ($stories as $story) : ?>
-        <?php
-        $title = $story['title'];
-        $img = $story['img'];
-        $article = $story['article'];
-        $reporter = $story['reporter'];
-        $date = $story['published'];
-        $shade = $story['shade'];
-        $layout = $story['layout'];
+  <?php foreach ($stories as $story) : ?>
+    <?php
+    $title = $story['title'];
+    $img = $story['img'];
+    $article = $story['article'];
+    $reporter = $story['reporter'];
+    $date = $story['published'];
+    $shade = $story['shade'];
+    $layout = $story['layout'];
 
-        ?>
+    ?>
 
-        <!-- .editorconfig, order by date, randomized like counter ID in authors  -->
+    <!-- .editorconfig, order by date, randomized like counter ID in authors  -->
 
-        <div class="article-container">
-            <div class="title-section">
-                <img class="dot1" src="/img/dot1.png">
-                <h2> <?php echo $title;  ?> </h2>
-            </div>
-            <br>
-            <!-- Here! -->
-            <div class="<?= $layout ?>">
-                <img class="article-img" src="<?= $img; ?>">
-                <br>
-                <span class=""><?= $article; ?></span>
-            </div>
-            <br>
-            <br>
-            <img src="/img/horizontalline.png">
-            <small class="article-info"><?php echo 'Reporter: ' . $reporter . ' | Published: ' . $date . ' | Shade: ' . $shade; ?></small>
-        </div>
-        <section>
-            - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        </section>
-    <?php endforeach; ?>
+    <div class="article-container media-query">
+      <div class="title-section media-query">
+        <img class="dot1 media-query" src="/img/dot1.png">
+        <h2> <?php echo $title;  ?> </h2>
+      </div>
+      <br>
+      <!-- Here! -->
+      <div class="<?= $layout ?> media-query">
+        <img class="article-img media-query" src="<?= $img; ?>">
+        <br>
+        <span class="article-text"><?= $article; ?></span>
+      </div>
+      <br>
+      <br>
+      <img class="horizontal-line media-query" src="/img/horizontalline.png">
+      <small class="article-info"><?php echo 'Reporter: ' . $reporter . ' | Published: ' . $date . ' | Shade: ' . $shade; ?></small>
+      <img class="horizontal-line media-query" src="/img/horizontalline.png" hidden>
+    </div>
+    <section class="media-query">
+      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    </section>
+  <?php endforeach; ?>
 </main>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
